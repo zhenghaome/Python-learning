e99be0731cc3cfe4d0221d8c28c9b1d3f7c70b69
Zhenghao <3501374214@qq.com>
2026-01-20 10:41:00 +0800

    文件读取练习

---
+++ Phase 1/文件操作/文件的读取-练习.py
@@ -0,0 +1,18 @@
+f = open("D:/Trivial Things/Python-learning/Phase 1/文件操作/word.txt", "r", encoding = "utf-8")
+
+# 方法一
+content = f.read()
+count = content.count("itheima")
+print(count)
+
+# 方法二
+count = 0
+for line in f:
+    line = line.strip() # 去除空格和\n
+    words = line.split(" ") # 按照空格分割字符串
+    for word in words:
+        if word == "itheima":
+            count += 1
+print(count)
+
+f.close()
