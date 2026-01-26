3e1c85e1eac47ebfc458a61d7cb60b0a5ab01fe1
Zhenghao <3501374214@qq.com>
2026-01-26 20:13:58 +0800

    冒泡排序

---
+++ Phase 2-算法/3 排序/3.1 冒泡排序.py
@@ -0,0 +1,23 @@
+def bubble_sort(li):
+    for i in range(len(li)-1):
+        for j in range(len(li)-1-i):
+            if li[j] > li[j+1]:
+                li[j], li[j+1] = li[j+1], li[j]
+            j += 1
+        i += 1
+    print(li)
+
+# 优化：如果一趟结束没有排序，说明本身就是排好序的，可以直接结束算法
+def bubble_sort2(li):
+    for i in range(len(li)-1):
+        exchenge = False
+        for j in range(len(li)-1-i):
+            if li[j] > li[j+1]:
+                li[j], li[j+1] = li[j+1], li[j]
+                exchange = True
+        print(li)
+        if not exchenge:
+            return
+
+bubble_sort2([1, 2, 3, 4, 5])
+
