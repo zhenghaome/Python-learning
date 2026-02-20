3c2bb95422dbb9ee42752d9e69b7831ce4828cb1
Zhenghao <3501374214@qq.com>
2026-02-20 08:26:15 +0800

    添加线程

---
+++ Phase 3-数据处理/3 爬虫/3.5 多线程/3.5.1 添加线程.py
@@ -0,0 +1,16 @@
+import threading
+
+# 添加线程：threading.Thread(target = )
+def thread_job():
+    print('这是新添加的线程')
+
+def main():
+    added_thread = threading.Thread(target = thread_job)
+    added_thread.start()   # 启动线程
+
+    print(threading.active_count())   # 算一下现在有多少个激活了的线程
+    print(threading.enumerate())   # 显示哪几个线程在运行
+    print(threading.current_thread())   # 显示一下当前正在运行哪个线程
+
+if __name__ == '__main__':
+    main()
